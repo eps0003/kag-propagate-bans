92979e3e2e7280a8b17ccf6109780dc50140b98a
epsilon <eps0003@users.noreply.github.com>
2021-03-01 21:09:59 +1100

    First working version

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import Server from "./server";
+
+new Server("127.0.0.1", 50301, "a");
+new Server("127.0.0.1", 50302, "a");
---
+++ src/server.ts
@@ -0,0 +1,135 @@
+import { Socket } from "net";
+
+const reconnectTime = 30000;
+
+export default class Server {
+	address: string;
+	port: number;
+	rconPassword: string;
+	socket = new Socket();
+
+	bans: string[] = [];
+	unbans: string[] = [];
+
+	static servers: Server[] = [];
+
+	constructor(address: string, port: number, rconPassword: string) {
+		this.address = address;
+		this.port = port;
+		this.rconPassword = rconPassword;
+
+		this.init();
+
+		Server.servers.push(this);
+	}
+
+	private init() {
+		this.bans = [];
+		this.unbans = [];
+
+		this.connect();
+
+		this.socket.on("end", () => {
+			console.log(`Connection ended with ${this.address}:${this.port}. Attempting to reconnect.`);
+			this.scheduleReconnect(reconnectTime);
+		});
+
+		this.socket.on("timeout", () => {
+			console.log(`Connection timed out with ${this.address}:${this.port}. Attempting to reconnect.`);
+			this.scheduleReconnect(reconnectTime);
+		});
+
+		this.socket.on("error", (err) => {
+			console.log(`${err}. Attempting to reconnect.`);
+			this.scheduleReconnect(reconnectTime);
+		});
+
+		this.socket.on("data", (buffer: Buffer) => {
+			const data = buffer.toString().trim();
+
+			const ban = this.parseBan(data);
+			if (ban) {
+				const index = this.bans.indexOf(ban.username);
+				if (index > -1) {
+					this.bans.splice(index, 1);
+				} else {
+					this.propagateBan(ban);
+				}
+			}
+
+			const unban = this.parseUnban(data);
+			if (unban) {
+				const index = this.unbans.indexOf(unban.username);
+				if (index > -1) {
+					this.unbans.splice(index, 1);
+				} else {
+					this.propagateUnban(unban);
+				}
+			}
+		});
+	}
+
+	private connect() {
+		this.socket = new Socket();
+		this.socket.connect({ host: this.address, port: this.port }, () => {
+			this.socket.write(`${this.rconPassword}\n`);
+			console.log(`Connected to ${this.address}:${this.port}`);
+		});
+	}
+
+	private scheduleReconnect(time: number) {
+		setTimeout(this.init.bind(this), time);
+	}
+
+	private parseBan(data: string): Ban | null {
+		if (data.startsWith("BAN")) {
+			const parts = data.split(" ");
+			return {
+				username: parts[1],
+				minutes: parseInt(parts[2]),
+				reason: parts.slice(3).join(" "),
+			};
+		}
+
+		return null;
+	}
+
+	private parseUnban(data: string): Unban | null {
+		if (data.startsWith("UNBAN")) {
+			const parts = data.split(" ");
+			return {
+				username: parts[1],
+			};
+		}
+
+		return null;
+	}
+
+	private propagateBan(ban: Ban) {
+		for (const server of Server.servers) {
+			if (this === server) continue;
+
+			server.bans.push(ban.username);
+			server.socket.write(`/ban ${ban.username} ${ban.minutes} ${ban.reason}\n`);
+		}
+	}
+
+	private propagateUnban(unban: Unban) {
+		for (const server of Server.servers) {
+			if (this === server) continue;
+
+			server.unbans.push(unban.username);
+			server.socket.write(`/unban ${unban.username}\n`);
+		}
+	}
+}
+
+interface Unban {
+	username: string;
+}
+
+interface Ban {
+	username: string;
+	minutes: number;
+	reason: string;
+}
